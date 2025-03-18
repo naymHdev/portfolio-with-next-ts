f88f72b16338aa97041bc16b8af67ef0724e9d1a
naymHdev <naym100m@gmail.com>
2025-03-18 22:45:06 +0600

    [updated] project card in fully refactoring in previous code

--- src/app/(commonLayout)/projects/page.tsx
+++ src/app/(commonLayout)/projects/page.tsx
@@ -5,6 +5,7 @@ import { fetchData } from "@/utils/fetchData";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { LiaArrowCircleRightSolid } from "react-icons/lia";
 
 export const metadata: Metadata = {
   title: "Projects | Naym Hossen",
@@ -18,51 +19,61 @@ const Projects = async () => {
   return (
     <>
       <RouteTitle firstP="My" secondP="Projects" />
-      <div className="mt-4 sm:mt-6 lg:mt-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {projects?.map((project) => (
-            <div
-              key={project._id}
-              className="border border-primaryColor bg-card custom-bg h-full flex flex-col"
-            >
-              <div>
-                <Image
-                  src={project.images[0]}
-                  alt={project?.projectName}
-                  layout="responsive"
-                  width={600}
-                  height={400}
-                  className="w-full h-auto object-cover rounded-t-lg"
-                />
-              </div>
-              <div className="p-3 mb-2 flex-grow flex flex-col">
-                <div className="flex items-center gap-3">
-                  <h2 className="text-2xl font-bold text-title">
-                    {project?.projectName}
-                  </h2>
-                </div>
-                <NHReactMarkdown
-                  content={
-                    project?.details?.length > 100
-                      ? project.details.slice(0, 100) + "..."
-                      : project?.details?.length < 10
-                      ? "Details not available"
-                      : project.details
-                  }
-                />
 
-                <div className="mt-auto flex justify-end">
+      {/* card ui design */}
+      <section className="">
+        <div>
+          {
+            projects?.length === 0 ? <><p>Projects are not available!</p></> : <>
+              {
+                projects?.map((project) => <div key={project._id}
+                  className="mt-5">
+                  <div className="flex flex-col md:flex-row justify-between w-full gap-6">
+                    <div className="flex-1">
+                      <NHReactMarkdown
+                        content={
+                          project?.details?.length > 100
+                            ? project.details.slice(0, 100) + "..."
+                            : project?.details?.length < 10
+                              ? "Details not available"
+                              : project.details
+                        }
+                      />
+                    </div>
+                    <div className="flex-1">
+                      <Image
+                        src={project.images[0]}
+                        alt={project?.projectName}
+                        layout="responsive"
+                        width={600}
+                        height={400}
+                        className="w-full h-auto object-contain rounded-lg"
+                      />
+                    </div>
+                  </div>
+
                   <Link href={`/projects/${project._id}`}>
-                    <button className="text-primaryColor font-extrabold text-sm bg-card border-none custom-bg px-3 py-2">
-                      Full Details
-                    </button>
+                    <div className="mt-4 cursor-pointer group">
+                      <h2 className="text-2xl font-bold text-title group-hover:text-primaryColor">{project?.projectName}</h2>
+
+                      <div className="flex items-center justify-between border-b border-[#013f5b] py-2 transition-all duration-300 group-hover:border-primaryColor">
+                        <p className="group-hover:text-primaryColor transition-colors duration-300">
+                          View project
+                        </p>
+                        <div className="text-primaryColor group-hover:translate-x-1 transition-transform duration-300">
+                          <LiaArrowCircleRightSolid fontSize={30} />
+                        </div>
+                      </div>
+                    </div>
                   </Link>
-                </div>
-              </div>
-            </div>
-          ))}
+
+                </div>)
+              }
+
+            </>
+          }
         </div>
-      </div>
+      </section>
     </>
   );
 };
